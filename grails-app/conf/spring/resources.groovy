f922c9af5b33ef8bb864d20bdbb678eab8ff2944
sdelamo <sergio.delamo@softamo.com>
2017-05-10 17:16:00 +0200

    Comment about issue

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,6 @@
 import es.virtualsw.webintegral.UserPasswordEncoderListener
 beans = {
+    // Until this issue is solved, you will need to comment out this bean definition to run any Grails command 
+    // https://github.com/grails/grails-data-mapping/issues/946
     userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
